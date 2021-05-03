345f1e59710f1bff2bd18cf7e4d5903343a60489
Hans Bakker <h.bakker@antwebsystems.com>
2021-05-03 16:30:05 +0700

    allow http comms to android mobile

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -7,7 +7,7 @@
 // ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
@@ -19,16 +19,17 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en';
 
-  static m0(classificationId) => "No ${classificationId} company found in system\n Go to the admin app to create one!";
+  static String m0(classificationId) =>
+      "No ${classificationId} company found in system\n Go to the admin app to create one!";
 
-  static m1(name) => "Welcome ${name}";
+  static String m1(name) => "Welcome ${name}";
 
-  static m2(screen) => "Screen ${screen} not found";
+  static String m2(screen) => "Screen ${screen} not found";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static _notInlinedMessages(_) => <String, Function> {
-    "classificationNotDefined" : m0,
-    "pageHomeWelcome" : m1,
-    "screenNotFound" : m2
-  };
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "classificationNotDefined": m0,
+        "pageHomeWelcome": m1,
+        "screenNotFound": m2
+      };
 }
