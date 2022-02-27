8b6c2fedb5dffa0317ac3290a5b70b45abc6f5ad
Hans Bakker <hans@Hanss-Mac-mini.local>
2022-02-27 12:34:00 +0700

    submitted to appstore 0.0.13/1

--- packages/admin/ios/Flutter/flutter_export_environment.sh
+++ packages/admin/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/dell/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/dell/growerp/packages/admin"
+export "FLUTTER_ROOT=/Users/hans/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hans/growerp/packages/admin"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.3"
-export "FLUTTER_BUILD_NUMBER=30"
+export "FLUTTER_BUILD_NUMBER=32"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
-export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "TREE_SHAKE_ICONS=true"
+export "PACKAGE_CONFIG=/Users/hans/growerp/packages/admin/.dart_tool/package_config.json"
